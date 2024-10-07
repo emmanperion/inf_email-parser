f10eb8ede7fe3b6bc5719b6f0e68e046b9a8f2d4
Emman Perion <ejgperion@outlook.com>
2024-10-07 22:58:02 +0800

    Added validation on update

---
+++ app/Http/Requests/SuccessfulEmailUpdate.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SuccessfulEmailUpdate extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return auth()->user()->tokenCan('api:successful-emails');
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'affiliate_id' => 'required|integer',
+            'envelope' => 'required|string',
+            'from' => 'required|string',
+            'subject' => 'required|string',
+            'dkim' => 'nullable|string',
+            'SPF' => 'nullable|string',
+            'spam_score' => 'nullable|numeric',
+            'email' => 'required|string',
+            'sender_ip' => 'nullable|string',
+            'to' => 'required|string',
+            'timestamp' => 'required|integer',
+        ];
+    }
+}
