4d59cee5f6e0630d005501175d8dc265012f4343
Emman Perion <ejgperion@outlook.com>
2024-10-07 22:30:38 +0800

    Updated migration

--- app/Models/SuccessfulEmail.php
+++ app/Models/SuccessfulEmail.php
@@ -4,11 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class SuccessfulEmail extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = [
         'affiliate_id',
         'envelope',
